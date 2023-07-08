de1a68f2ed892d82ec94987c89af460634c015f6
dev-hyunjin-park <136670876+dev-hyunjin-park@users.noreply.github.com>
2023-07-09 00:45:27 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# marquee-infinite-horizontal-scrolling
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-bhqjxv)
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div className="marquee-container">
+      <div className="marquee-text">
+        You spin me right round, baby. Like a record, baby.
+      </div>
+    </div>
+  );
+}
